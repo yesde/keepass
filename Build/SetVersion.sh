07f6babfe39cac7cadbc4b5e3c85182864e13bef
Will Rouesnel <w.rouesnel@gmail.com>
2016-03-27 00:15:31 +1100

    SetVersion fallback if upstream not avail

--- Build/SetVersion.sh
+++ Build/SetVersion.sh
@@ -4,6 +4,9 @@
 # Get the version in the upstream branch
 
 upstreamVersion=$(git show upstream:KeePassLib/PwDefs.cs | grep VersionString | grep -o '".*"' | grep -o '[^"]*')
+if [ -z $upstreamVersion ]; then
+    upstreamVersion=2.unknown
+fi
 commitish=$(git rev-parse --short HEAD)
 
 version=$upstreamVersion~$USER-$commitish
